642513482bf4ba6c7cea9291e68c4ccb851ab34c
Mugdha Joshi <mugdha.joshi@cerner.com>
2018-04-26 11:44:46 -0400

    This is a second commit

--- src/com/cerner/Practice1.java
+++ src/com/cerner/Practice1.java
@@ -3,5 +3,6 @@ package com.cerner;
 public class Practice1 {
     public static void main(String[] args) {
         System.out.println("Hello, Practice1");
+        System.out.println("This is just an added comment");
     }
 }
